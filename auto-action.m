d82abaa8b22e8fc0ca54c756969f2078e3686f4d
Andreas-Pieper <andreas.pieper@univ-rennes1.fr>
2024-04-08 12:16:30 -0400

    Working on normalizing CM action

--- auto-action.m
+++ auto-action.m
@@ -32,79 +32,33 @@ end function;
 // copy pasta
 /*
 load "auto-action.m";
-Pi, S := AutoAction();
-Pi;
 M, S := AutoAction();
 Pi := BigPeriodMatrix(S);
-Nrows(M);
-M^15;
-M^5;
-M^3;
 K<z> := CyclotomicField(15);
-Codifferent;
-Dinv := Codifferent(K);
-D := Different(K);
 OK := Integers(K);
-Dinv := Codifferent(K);
-Dinv := Codifferent(OK);
 D := Different(OK);
-D^-1;
-Dinv := $1;
-IsPrincipal(Dinv);
-_, d := IsPrincipal(Dinv);
-d;
-IsTotallyReal(d*I);
-[Evaluate(d,v) : v in InfinitePlaces(K)];
-[Re(Evaluate(d,v)) : v in InfinitePlaces(K)];
-IsNarrowlyPrincipal(Dinv);
-_, d:= $1;
-[Im(Evaluate(d,v)) : v in InfinitePlaces(K)];
-_, d := IsPrincipal(Dinv);
-d;
-ComplexConjugate(d);
-d*ComplexConjugate(d);
-IsTotallyReal($1);
-[Im(Evaluate(d*ComplexConjugate(d),v)) : v in InfinitePlaces(K)
-];
-IsTotallyPositive(d*ComplexConjugate(d));
-TotallyRealSubAlgebra;
-sub< K | d*ComplexConjugate(d) >;
-Kre, res := $1;
-Degree(K);
-Degree(Kre);
-K.1;
-sub< K | K.1*ComplexConjugate(K.1) >;
-Kre, res := $1;
-Degree(Kre);
-sub< K | K.1+ComplexConjugate(K.1) >;
-Kre, res := $1;
-Degree(Kre);
+Dinv := D^-1;
+Kre, res := sub< K | K.1+ComplexConjugate(K.1) >;
 OKre := Integers(Kre);
-Dreinv := Codifferent(OKre);
 Dre := Different(OKre);
 Dreinv := Dre^-1;
-Dreinv;
-IsNarrowlyPrincipal(Dreiv);
-IsNarrowlyPrincipal(Dreinv);
-_, dre := $1;
-MinimalPolynomial(K.1,Kre);
-Discriminant($1);
-disc := $1;
-dre/disc;
-R<x> := PolynomialRing(K);
-Roots(K, x^2 - disc);
-Roots(x^2 - disc,K);
-Roots(x^2 - disc,K)[2][1];
-r := $1;
-dre/r;
-d_new := $1;
-[Re(Evaluate(d_new,v)) : v in InfinitePlaces(K)];
-ideal< OK | d_new > eq Dinv;
-M := [];
-for i := 1 to Rank(OK) do
-for j := 1 to Rank(OK) do
-for i := 1 to Rank(OK) do
-M := [Trace(b[i]*ComplexConjugate(b[j])*d_new) : i, j in [1..Rank(OK)]] where b := [OK.i : i in [1..Rank(OK)]];
-M := [Trace(b[i]*ComplexConjugate(b[j])*d_new) : i, j in [1..Degree(K)]] where b := [OK.i : i in [1..Degree(K)]];
-M := Matrix(8,8,M);
+_, dre := IsNarrowlyPrincipal(Dreinv);
+mini := MinimalPolynomial(K.1,Kre);
+disc := Discriminant(mini);
+d_new := dre/Sqrt(K!disc);
+assert ideal< OK | d_new > eq Dinv;
+Pair := [Integers()!Trace(b[i]*ComplexConjugate(b[j])*d_new) : i, j in [1..Degree(K)]] where b := [OK.i : i in [1..Degree(K)]];
+Pair := Matrix(8,8,Pair);
+id4 := IdentityMatrix(Integers(), 4);
+zer := ZeroMatrix(Integers(), 4,4);
+J := BlockMatrix([[zer, id4], [-id4, zer]]);
+v:= Vector([(J*M^i)[1,1]: i in [0..7]]);
+X:= Matrix([[Integers()!Trace(K.1^(-i)*OK.j*d_new): i in [0..7]]: j in [1..8] ]);
+sol := v*X^-1;
+nrm := OK!Eltseq(sol);
+
+//Tr(zeta^i *a* conjugate(a) * d_new)
+
+
+
 */
