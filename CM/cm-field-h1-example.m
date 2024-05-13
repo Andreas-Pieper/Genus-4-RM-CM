352132b46880346595cbbba9d1cd9d21974ef600
Sam Schiavone <sschiavo@mit.edu>
2024-05-13 12:21:19 -0400

    one more spec

--- CM/cm-field-h1-example.m
+++ CM/cm-field-h1-example.m
@@ -1,7 +1,8 @@
-AttachSpec("~/github/cm-calculations-g3/magma/spec");
 AttachSpec("~/github/CHIMP/CHIMP.spec");
+AttachSpec("~/github/cm-calculations-g3/magma/spec");
 AttachSpec("~/github/Genus-4/magma/spec");
 AttachSpec("~/github/reconstructing-g4/magma/spec");
+AttachSpec("~/github/Reconstruction/magma/spec");
 Attach("findg4.m");
 load "algebraize.m";
 SetVerbose("CMExp",2);
