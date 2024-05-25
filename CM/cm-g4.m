7038b6808149a11701b8b80908a7c53242360a97
Sam Schiavone <sschiavo@mit.edu>
2024-05-24 20:00:26 -0400

    fix bugs in script

--- CM/cm-g4.m
+++ CM/cm-g4.m
@@ -1,31 +1,27 @@
-AttachSpec("~/github/CHIMP/CHIMP.spec");
-AttachSpec("~/github/cm-calculations-g3/magma/spec");
-AttachSpec("~/github/Genus-4/magma/spec");
-AttachSpec("~/github/reconstructing-g4/magma/spec");
-AttachSpec("~/github/Reconstruction/magma/spec");
-AttachSpec("spec");
-
-intrinsic InitializeDataFiles() -> .
+intrinsic InitializeDataFiles() -> Any 
   {}
 
   all_filename := "cm-fields-schottky-values.txt";
   jac_filename := "cm-fields-jacobians.txt";
   for name in [all_filename, jac_filename] do
     System(Sprintf("touch %o", name));
-    Write(name, "Format: label, coeffs, index in list of taus, absolute value of Schottky form");
+    Write(name, "Format: label, coeffs, index in list of taus, absolute value of Schottky form\n");
   end for;
-  return Sprintf("Files %o and %o\n initialized\n", all_filename, jac_filename);
+  return Sprintf("Files %o and %o initialized", all_filename, jac_filename);
 end intrinsic;
 
-intrinsic CheckForJacobians(label::MonStgElt, coeffs::SeqEnum, filename::MonStgElt : prec := 40) -> .
+intrinsic CheckForJacobians(label::MonStgElt, coeffs_str::MonStgElt : prec := 40) -> Any
   {}
 
+  coeffs := eval coeffs_str;
   QQ := RationalsExtra(prec);
   CC<I> := QQ`CC;
   eps := CC`epscomp;
   R<x> := PolynomialRing(QQ);
   f := R!coeffs;
   taus := FullEnumerationG4(f);
+  all_filename := "cm-fields-schottky-values.txt";
+  jac_filename := "cm-fields-jacobians.txt";
   for i->tau in taus do
     tau_red := SiegelReduction(tau);
     sch := SchottkyModularForm(tau_red : prec := prec);
@@ -38,28 +34,3 @@ intrinsic CheckForJacobians(label::MonStgElt, coeffs::SeqEnum, filename::MonStgE
   end for;
   return Sprintf("Data written to %o and %o\n", all_filename, jac_filename);
 end intrinsic;
-
-prec := 300;
-//8.0.1265625.1
-//coeffs := [1, -1, 0, 1, -1, 1, 0, -1, 1];
-//8.0.13140625.1
-coeffs := [1, 3, 5, 3, 4, -3, 5, -3, 1];
-QQ := RationalsExtra(prec);
-CC<I> := QQ`CC;
-eps := CC`epscomp;
-R<x> := PolynomialRing(QQ);
-f := R!coeffs;
-K<nu> := NumberFieldExtra(f);
-_, K0incl, inv := IsCMField(K);
-K0, incl := Explode(K0incl);
-taus := FullEnumerationG4(f);
-tau := taus[1];
-// reducing precision
-tau0 := tau;
-tau := ChangeRing(tau, CC);
-print "checking if Jacobian";
-tau_red := SiegelReduction(tau);
-sch := SchottkyModularForm(tau_red : prec := (prec div 3));
-print sch;
-assert Abs(sch) lt 10^(-prec/4);
-
--- CM/cm-script.m
+++ CM/cm-script.m
@@ -1,5 +1,5 @@
 /*
-  Usage: Make an input file input.txt each of whose rows consists of an LMFDB field label and an LMFDB ideal label separated by a space using GenerateFieldsAndLevels. E.g., 
+  Usage: Make an input file cm-fields-deg-8-h-1.txt each of whose rows consists of an LMFDB field label and an LMFDB ideal label separated by a space using GenerateFieldsAndLevels. E.g., 
 
   8.0.1265625.1|[1, -1, 0, 1, -1, 1, 0, -1, 1]
   8.0.4000000.1|[1, 0, -1, 0, 1, 0, -1, 0, 1]
@@ -7,9 +7,14 @@
 
   Then run the following command in the directory ~/github/Genus-4-RM-CM/CM
 
-  parallel -j 16 --joblog joblog --eta --colsep '|' -a input.txt magma -b label:={1} coeffs:={2} ModFrmHilD/CanonicalRing/CanonicalRingsScript.m
+parallel -j 16 --joblog joblog --eta --colsep '|' -a cm-fields-deg-8-h-1.txt magma -b label:={1} coeffs:={2} cm-script.m
 */
 
+AttachSpec("~/github/CHIMP/CHIMP.spec");
+AttachSpec("~/github/cm-calculations-g3/magma/spec");
+AttachSpec("~/github/Genus-4/magma/spec");
+AttachSpec("~/github/reconstructing-g4/magma/spec");
+AttachSpec("~/github/Reconstruction/magma/spec");
 AttachSpec("spec");
 
 try 
