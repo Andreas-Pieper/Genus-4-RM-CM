cac471449e85ab6eec749d04b1490a90b3849f23
Thittho <bouchetthomas@orange.fr>
2024-09-24 17:24:30 +0200

    fix minimize on number fields

--- CM/minimize.m
+++ CM/minimize.m
@@ -168,31 +168,56 @@ D1 := &*Coefficients(Q1);
 D;
 D1;
 */
-K := NumberFieldExtra(PolynomialRing(QQ)![1,-1,-4,0,1] : prec := 300); 
+/*K := NumberFieldExtra(PolynomialRing(QQ)![1,-1,-4,0,1] : prec := 300); 
 OK := RingOfIntegers(K);
 S<X,Y,Z,T> := PolynomialRing(K, 4);
 Q := 1/128625*(575000*K.1^3 + 221900*K.1^2 - 2208810*K.1 - 1419318)*X^2 + 1/4501875*(-153115500*K.1^3 - 60826800*K.1^2 + 588431340*K.1 + 387024964)*X*Y + 1/157565625*(9159221750*K.1^3 + 3628138250*K.1^2 - 35197618575*K.1 - 23100730811)*X*Z + 1/315131250*(9159221750*K.1^3 + 3628138250*K.1^2 - 35197618575*K.1 - 23100730811)*Y^2 + 1/5514796875*(859371821500*K.1^3 + 340574405200*K.1^2 - 3302487234240*K.1 - 2168159942836)*Y*Z + 1/386035781250*(-42873025928375*K.1^3 - 16992558908450*K.1^2 + 164757508535595*K.1 + 108174078005139)*Z^2;
 E := 1/157565625*(13058247000*K.1^3 + 5172827250*K.1^2 - 50181188625*K.1 - 32935574353)*X^3 + 1/11029593750*(1590636346250*K.1^3 + 630207030350*K.1^2 - 6112630599645*K.1 - 4012401111193)*X^2*Y + 1/64339296875*(830526951625*K.1^3 + 328717613650*K.1^2 - 3191541106215*K.1 - 2093665398753)*X^2*Z + 1/64339296875*(830526951625*K.1^3 + 328717613650*K.1^2 - 3191541106215*K.1 - 2093665398753)*X*Y^2 + 1/6755626171875*(-17827862551428875*K.1^3 - 7065902545205600*K.1^2 + 68510982690997815*K.1 + 44981533906551157)*X*Y*Z + 1/472893832031250*(-188877559323955375*K.1^3 - 74859896879675275*K.1^2 + 725840666494777545*K.1 + 476557844374624832)*X*Z^2 + 1/13511252343750*(-10227561207178375*K.1^3 - 4053586423233325*K.1^2 + 39303658442463855*K.1 + 25805154188013094)*Y^3 + 1/236446916015625*(1071473607310420750*K.1^3 + 424666091100292675*K.1^2 - 4117582772521909665*K.1 - 2703429618689275709)*Y^2*Z + 1/22068378828125000*(-23183327866866549000*K.1^3 - 9188452638183550125*K.1^2 + 89091578053401622950*K.1 + 58493777103891816551)*Y*Z^2 + 1/579294944238281250*(-4363276308376373695625*K.1^3 - 1729334545308948675800*K.1^2 + 16767703335710978781960*K.1 + 11008963299922388808644)*Z^3 + T^3;
+*/
+K := NumberFieldExtra(PolynomialRing(QQ)![-2,-11,-13,-1,1] : prec := 300); 
+OK := RingOfIntegers(K);
+S<X,Y,Z,T> := PolynomialRing(K, 4);
+Q := 1/1895803875*(-113192379240*K.1^3 - 393317496940*K.1^2 - 289176226020*K.1 - 50465818102)*X^2 + 1/199059406875*(27909354977480*K.1^3 + 97016599239280*K.1^2 + 71399792929240*K.1 + 12486656012488)*X*Y + 1/9593668114340625*(63564159650145615200*K.1^3 + 220940077675254027200*K.1^2 + 162562718109700509600*K.1 + 28402911703969489776)*X*Z + 1/9593668114340625*(31782079825072807600*K.1^3 + 110470038837627013600*K.1^2 + 81281359054850254800*K.1 + 14201455851984744888)*Y^2 + 1/10782216997395046875*(2318375735664822965564640*K.1^3 + 8058349296616251492213440*K.1^2 + 5929157904454592826599520*K.1 + 1035946612557618344216096)*Y*Z + 1/10189195062538319296875*(1344547221932520565005812480*K.1^3 + 4673457773144208550537008480*K.1^2 + 3438628374280220932880979840*K.1 + 600799493520328936501787296)*Z^2;
+E := 1/9593668114340625*(121863315039620964500*K.1^3 + 423579740878817711000*K.1^2 + 311660512091604493500*K.1 + 54453420629511691548)*X^3 + 1/97039952976555421875*(60847943026249576628246240*K.1^3 + 211498925271436643186755040*K.1^2 + 155616299759256930918736320*K.1 + 27189386374042105683629016)*X^2*Y + 1/10189195062538319296875*(-1106280940364585110919801360*K.1^3 - 3845277558233094218003667360*K.1^2 - 2829271437963319159976180880*K.1 - 494332251981999769089831952)*X^2*Z + 1/10189195062538319296875*(-1106280940364585110919801360*K.1^3 - 3845277558233094218003667360*K.1^2 - 2829271437963319159976180880*K.1 - 494332251981999769089831952)*X*Y^2 + 1/18187713186630899944921875*(-637928338556078404881716107588480*K.1^3 - 2217349533528498274674952144434880*K.1^2 - 1631477468763782701802479767431040*K.1 - 285052862357244240830245737885504)*X*Y*Z + 1/551906156648314658828654296875*(-596909276718814918907335327825035328640*K.1^3 - 2074773021194512542636924219366595507040*K.1^2 - 1526572777849809630228108740565380872320*K.1 - 266723842809660003442969394065530110784)*X*Z^2 + 1/54563139559892699834765625*(627599312497303610636041802445760*K.1^3 + 2181447286028790938403344806562560*K.1^2 + 1605061377442051416913233111924480*K.1 + 280437424793363654627775196657248)*Y^3 + 1/551906156648314658828654296875*(345549311080612538902173117593800111360*K.1^3 + 1201080995194575522607110146908985272960*K.1^2 + 883729223643035265672911062807608967680*K.1 + 154405776097231187297102292544687089216)*Y^2*Z + 1/30679489296038667799592841796875*(113299791831126545439035406007565690355200*K.1^3 + 393814203542554807994838122654869906491200*K.1^2 + 289759909405693717155500644765548234185600*K.1 + 50627050115307238388599847225698887706496)*Y*Z^2 + 1/310323034229431124792881594775390625*(17245553711074802706516180557963752986578304320*K.1^3 + 59943128664349286718267114167330793651451550720*K.1^2 + 44104847857266964863100849611965491468806413760*K.1 + 7706029268774172986616484993717003332487659648)*Z^3 + T^3;
 
+
+function NumberFieldLCM(L)
+  if #L eq 0 then
+    return 1;
+  end if;
+  K := Parent(L[1]);
+  OK := Integers(K);
+  if K eq Rationals() then
+    return LCM(ChangeUniverse(L, Integers()));
+  end if;
+  I := [OK*x : x in L];
+  _, a := IsPrincipal(&meet I);
+  return K!a;
+end function;
+
+function NumberFieldGCD(L)
+  if #L eq 0 then
+    return 1;
+  end if;
+  K := Parent(L[1]);
+  OK := Integers(K);
+  if Type(K) eq FldRat then
+    return GCD(ChangeUniverse(L, Integers()));
+  elif Type(K) eq FldNum then
+    I := [OK*x : x in L];
+    _, a := IsPrincipal(&+I);
+    return K!a;
+  end if;
+  I := [OK*x : x in L];
+  _, a := IsPrincipal(&+I);
+  return K!a;
+end function;
 
 function ElimGCD(F)
-    for d in Decomposition(Coefficients(F)[1]) do
-        _, p := IsPrincipal(d[1]);
-        F := F div p^(Min([Valuation(c, d[1]) : c in Coefficients(F)]));
-    end for;
-    return F;
+    return F div NumberFieldGCD(Coefficients(F));
 end function;
 
-function Gcd(F)
-    res := 1;
-    for d in Decomposition(Coefficients(F)[1]) do
-        _, p := IsPrincipal(d[1]);
-        res *:= p^(Min([Valuation(c, d[1]) : c in Coefficients(F)]));
-    end for;
-    return res;
-end function;
-
-function MinimizeSQuadric(Q, E)
+function MinimizeQuadric(Q, E)
     OK := RingOfIntegers(BaseRing(Parent(Q)));
     S := PolynomialRing(OK, Rank(Parent(Q)));
     
@@ -269,7 +294,7 @@ TestRedMinimization := function(f,p,m,h)
   if TotalDegree(tmp[1]) eq 1 then
    tran := SubsForLFac(tmp[1], Parent(f), p, m, h);
    res := Evaluate(f,tran);
-   gcd := Gcd(res);
+   gcd := NumberFieldGCD(Coefficients(res));
    try  /* Equation gets divisible by the transformation */
     res := res div gcd;
    catch e
@@ -353,11 +378,12 @@ end function;
 CritPointsOnLine := function(line,f,p,m,h)
  local r2, akt, tran, rf, fac, res, r2p, gcd, tmp, i;
 
- r2 := Parent(f);
+ 
+ r2 := PolynomialRing(BaseRing(Parent(f)), 2);
  tran := NormalizeLine(line, PolynomialRing(BaseRing(Parent(f)),4), m);
  tran := [Evaluate(akt,[0,0,r2.1,r2.2]) : akt in tran]; 
  rf := Evaluate(f,tran);
- gcd := Gcd(rf);
+ gcd := NumberFieldGCD(Coefficients(rf));
  try
   rf := rf div p;
  catch e
@@ -374,13 +400,22 @@ CritPointsOnLine := function(line,f,p,m,h)
   vprintf MinRedCubSurf,2:"Success\n";
  end try;
 
- r2p := Codomain(h);
+Fp := Codomain(m);
+Fpx := PolynomialRing(Fp, 2);
+if IsPrime(#Fp) then
+    inc := hom<Fp -> Fpx | >;
+else
+    inc := hom<Fp -> Fpx | Fp.1>;
+end if;
+h := hom<r2 -> Fpx | m*inc,[Fpx.i : i in [1..2]]>;
+
+r2p := Codomain(h);
 
  fac := Factorization(h(rf)); 
  res := [];
  for akt in fac do
   if Degree(akt[1]) eq 1 then
-   tmp := [MonomialCoefficient(akt[1],r2p.2), -MonomialCoefficient(akt[1],r2p.1)];
+   tmp := [MonomialCoefficient(akt[1],r2p.2) @@ m, -MonomialCoefficient(akt[1],r2p.1) @@ m];
    Append(~res,[Evaluate(tran[i],tmp) : i in [1..4]]);
   end if;
  end for;
@@ -436,13 +471,15 @@ ListCriticalPoints :=  function(f, lines, p, m, h)
  local crit_pnt,rp3, gb,pt,akt, suc;
  
  crit_pnt := ListIsolatedSingularPoints(h(f));
+ crit_pnt := [c @@ m : c in crit_pnt];
  for line in lines do
-  if p le 3 then
+  if Characteristic(Codomain(m)) le 3 then
    crit_pnt := crit_pnt cat PointsOnLine(line,m);
   else
    crit_pnt := crit_pnt cat CritPointsOnLine(line,f,p,m,h);
   end if;
  end for;
+ crit_pnt := [m(c) : c in crit_pnt];
 
 // if IsIrreducible(h(f)) and (not IsIrreducible(PolynomialRing(GF(p^3),4)!f)) then  // we don't do it here
 /* We have 3 conjugate planes */
@@ -495,7 +532,7 @@ TryToComplete := function(f,p,m,h)
    tran := NormalizeLine(gb,Parent(f),m);
    tran := [Evaluate(Evaluate(tmp,1,p*Parent(f).1),2,p*Parent(f).2) : tmp in tran];
    res := Evaluate(f,tran);
-   gcd := Gcd(res);
+   gcd := NumberFieldGCD(Coefficients(res));
    
    try  /* Equation gets divisible by the transformation */
     _ := gcd div p;
@@ -523,7 +560,7 @@ TryToComplete := function(f,p,m,h)
  tran := SubsForLFac(l_fac[1], Parent(f), p, m, h);
   
  ttc := Evaluate(f,tran);
- gcd := Gcd(ttc);
+ gcd := NumberFieldGCD(Coefficients(ttc));
  
  try  /* Equation gets divisible by the transformation */
   _ := gcd div p;
@@ -561,7 +598,7 @@ TryToComplete := function(f,p,m,h)
   tran_2 := [Evaluate(Evaluate(tmp,1,p*Parent(f).1),2,p*Parent(f).2) : tmp in tran_2];
   
   ttc_2 := Evaluate(ttc,tran_2);
-  gcd := Gcd(ttc_2);
+  gcd := NumberFieldGCD(Coefficients(ttc_2));
   try  /* Equation gets divisible by the transformation */
    _ := gcd div p;
   catch e
@@ -584,7 +621,7 @@ TryToComplete := function(f,p,m,h)
    if TotalDegree(tmp[1]) eq 1 then
     tran_3 := SubsForLFac(tmp[1], Parent(f), p, m, h); 
     ttc_3 := Evaluate(ttc_2,tran_3);
-    gcd := Gcd(ttc_3);
+    gcd := NumberFieldGCD(Coefficients(ttc_3));
     try  /* Equation gets divisible by the transformation */
      _ := gcd div p;
     catch e
@@ -631,7 +668,7 @@ LocalMinimization := function(f,p,m,h)
   tran := [Evaluate(akt,1,p*Parent(akt).1) : akt in tran];
   tran := [Evaluate(akt,2,p*Parent(akt).2) : akt in tran];
   res := Evaluate(f,tran);
-  gcd := Gcd(res);
+  gcd := NumberFieldGCD(Coefficients(res));
   try 
    _ := gcd div (p^2);
    vprintf MinRedCubSurf,2:"Success using weight vector (0,0,1,1). \n";   
@@ -649,14 +686,14 @@ LocalMinimization := function(f,p,m,h)
 /* crit_pnt; */
 /* printf"Trying remaining weight-vectors\n"; */
  for i := 1 to #crit_pnt do
-  tran := NormalizePoint(crit_pnt[i], Parent(f), m);
+  time tran := NormalizePoint(crit_pnt[i], Parent(f), m);
 /*  printf "Normalize singular point by %o\n",tran;  */
   tran := [Evaluate(akt,[Parent(f).1, Parent(f).2*p, Parent(f).3*p, Parent(f).4*p]) : akt in tran];
-  res := Evaluate(f, tran);
-  gcd := Gcd(res);
+  time res := Evaluate(f, tran);
+  time gcd := NumberFieldGCD(Coefficients(res));
 /*  printf "gcd %o found\n",gcd; */
   try 
-   _ := gcd div (p^3);    
+   time _ := gcd div (p^3);    
    vprintf MinRedCubSurf,2:"Success using weight vector (0,1,1,1). \n";   
    return (res div gcd),tran;
   catch e
@@ -689,14 +726,15 @@ function MinimizeCubicSurfaceNbFld(f, d)
         inc := hom<Fp -> Fpx | Fp.1>;
     end if;
     h := hom<S -> Fpx | m*inc,[Fpx.i : i in [1..4]]>;
-
     res := ElimGCD(f);
     tran := [Parent(f).i : i in [1..4]];
-    repeat
-    res,t_neu := LocalMinimization(res,p,m,h);
-    tran := [Evaluate(akt,t_neu) : akt in tran];
-    until (t_neu eq [Parent(f).i : i in [1..4]]);
 
+    if Characteristic(Fp) ge 5 then 
+        repeat
+        res,t_neu := LocalMinimization(res,p,m,h);
+        tran := [Evaluate(akt,t_neu) : akt in tran];
+        until (t_neu eq [Parent(f).i : i in [1..4]]);
+    end if;
     /* Simplify transformation by using LLL */
     //Lat := LLL(Matrix([[MonomialCoefficient(tran[j],Parent(f).i) : j in [1..#tran]] : i in [1..Rank(Parent(f))]]));
     //Lat := Transpose(Lat);
@@ -742,7 +780,7 @@ if inv ne 0 then
 end if;
 
 SetDebugOnError(true);
-
+SetVerbose("MinRedCubSurf", 2);
 for i->d in l_p do
     E1, T := MinimizeCubicSurfaceNbFld(E1, d);
     Q1 := ElimGCD(Evaluate(Q1, T));
